dc191adbaedfb5688020a3c57cabc82ed42794be
Rodrigo <leycourcino@gmail.com>
2019-07-04 02:33:36 -0500

    Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,30 @@
 import React,{Component} from 'react'
+import axios from 'axios'
 
 class App extends Component{
   constructor(){
     super()
     this.state={
-        usuarios:[
-          {
-            nombre:"Rodrigo",
-            correo:"Rodrigo@gmail.com",
-            enlace:"rodrigo.com"
-          },
-          {
-            nombre:"Platzi",
-            correo:"Paltzi@Platzi",
-            enlace:"platzi.com"
-          }
-        ]
+        usuarios:[]
     }
   }
+  async componentDidMount(){
+    const respuesta = await axios.get('https://jsonplaceholder.typicode.com/users')
+    
+    this.setState({
+      usuarios: respuesta.data})
+  }
   ponerFila=()=>(
-    this.state.usuarios.map( (usuario,index)=>(
-      <tr>
-        <td>{usuario.nombre}</td>
-        <td>{usuario.correo}</td>
-        <td>{usuario.enlace}</td>
+    this.state.usuarios.map( (usuario)=>(
+      <tr key = {usuario.id}>
+        <td >{usuario.name}</td>
+        <td  >{usuario.email}</td>
+        <td  >{usuario.website}</td>
       </tr>
     )   )
   )
 render(){
+  
   return(
     <div className="margen">
        <table className="tabla">
